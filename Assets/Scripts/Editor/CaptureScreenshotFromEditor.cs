950f99076070371da0a215d81c444996d3531456
川辺 兼嗣 <kawanabe_kenji@grenge.co.jp>
2022-07-13 13:57:56 +0900

    撮影機能追加

---
+++ Assets/Scripts/Editor/CaptureScreenshotFromEditor.cs
@@ -0,0 +1,29 @@
+using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Unityエディタ上からGameビューのスクリーンショットを撮るEditor拡張
+/// </summary>
+public class CaptureScreenshotFromEditor : Editor
+{
+    /// <summary>
+    /// キャプチャを撮る
+    /// </summary>
+    /// <remarks>
+    /// Tool > Screenshot に追加。
+    /// HotKeyは Ctrl + Shift + F1。
+    /// </remarks>
+    [MenuItem("Tool/Screenshot #%F1")]
+    private static void CaptureScreenshot()
+    {
+        var folderPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+        var fileName = DateTime.Now.ToString("スクリーンショット yyyy-MM-dd HH.mm.ss") + ".png";
+        var exportPath = string.Format("{0}/{1}", folderPath, fileName);
+        ScreenCapture.CaptureScreenshot(exportPath);
+        File.Exists(exportPath);
+
+        Debug.Log("ScreenShot: " + fileName);
+    }
+}
